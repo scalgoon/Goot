c9d759563937e0c57d38993a8f8a01667d71d1ae
Gooselad <63563412+scalgoon@users.noreply.github.com>
2021-11-26 17:48:12 -0600

    Tags added

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const  { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
@@ -8,94 +8,143 @@ module.exports = async (client, message) => {
     if (!message.guild ||
         message.author.bot) return;
 
-        const prefix = "!"
+    const prefix = "!"
 
-        const prefixRegex = new RegExp(`^(${escapeRegex(prefix)})\\s*`);
+    const prefixRegex = new RegExp(`^(${escapeRegex(prefix)})\\s*`);
 
-        if (!prefixRegex.test(message.content)) return;
+    if (!prefixRegex.test(message.content)) return;
 
-        const [ matchedPrefix ] = message.content.match(prefixRegex);
+    const [matchedPrefix] = message.content.match(prefixRegex);
 
-        const args = message.content.slice(matchedPrefix.length).trim().split(/ +/g); 
+    const args = message.content.slice(matchedPrefix.length).trim().split(/ +/g);
 
-        const textExists = await prisma.cCommands.findUnique({
-            where: {
-                placeholder: `${args[0] + message.guild.id}`
-            },
-            select: {
-                text: true,
-                guild: true,
-                deltrig: true,
-                permission: true
+    const textExists = await prisma.cCommands.findUnique({
+        where: {
+            placeholder: `${args[0] + message.guild.id}`
+        },
+        select: {
+            text: true,
+            guild: true,
+            deltrig: true,
+            permission: true
+        }
+    })
+
+    if (textExists) {
+
+        if (textExists.guild === message.guild.id) {
+
+            if (textExists.permission === "staff") {
+                if (!message.member.permissions.has("MANAGE_MESSAGES")) return;
             }
-        })
 
-        if(textExists) {
+            if (textExists.permission === "admin") {
+                if (!message.member.permissions.has("ADMINISTRATOR")) return;
+            }
 
-            if(textExists.guild === message.guild.id) {
 
-                if(textExists.permission === "staff") {
-                    if(!message.member.permissions.has("MANAGE_MESSAGES")) return;
+            let obj = {
+                $text: {
+                    value: args.splice(1).join(" ")
+                },
+                $userID: {
+                    value: message.author.id
+                },
+                $serverID: {
+                    value: message.guild.id
+                },
+                $user: {
+                    value: message.mentions.users.first() || ""
+                },
+                $channel: {
+                    value: message.mentions.channels.first() || ""
+                },
+                $nl: {
+                    value: "\n"
                 }
+            }
 
-                if(textExists.permission === "admin") {
-                    if(!message.member.permissions.has("ADMINISTRATOR")) return;
-                }
+            let newtext = textExists.text.replace("$text", obj.$text.value).replace("$userID", obj.$userID.value).replace("$serverID", obj.$serverID.value).replace("$user", obj.$user.value).replace("$channel", obj.$channel.value).replace("$nl", obj.$nl.value)
 
-                if(textExists.deltrig === true) {
-                    message.delete();
-                    message.channel.send(`${textExists.text}`);
-                    return;
-                } else {
-                    message.reply(`${textExists.text}`);
-                }
-            
+
+            if (textExists.deltrig === true) {
+                message.delete();
+                message.channel.send(`${newtext}`);
+                return;
+            } else {
+                message.reply(`${newtext}`);
             }
 
         }
 
-        const embedExists = await prisma.cCEmbeds.findUnique({
-            where: {
-                placeholder: `${args[0] + message.guild.id}`
+    }
+
+    const embedExists = await prisma.cCEmbeds.findUnique({
+        where: {
+            placeholder: `${args[0] + message.guild.id}`
+        },
+        select: {
+            description: true,
+            guild: true,
+            title: true,
+            deltrig: true,
+            permission: true
+        }
+    })
+
+    if (embedExists) {
+
+        if (embedExists.permission === "staff") {
+            if (!message.member.permissions.has("MANAGE_MESSAGES")) return;
+        }
+
+        if (embedExists.permission === "admin") {
+            if (!message.member.permissions.has("ADMINISTRATOR")) return;
+        }
+
+        const obj = {
+            $text: {
+                value: args.splice(1).join(" ")
             },
-            select: {
-                description: true,
-                guild: true,
-                title: true,
-                deltrig: true,
-                permission: true
+            $userID: {
+                value: message.author.id
+            },
+            $serverID: {
+                value: message.guild.id
+            },
+            $user: {
+                value: message.mentions.users.first() || ""
+            },
+            $channel: {
+                value: message.mentions.channels.first() || ""
+            },
+            $nl: {
+                value: "\n"
             }
-        })
+        }
 
-        if(embedExists) {
+        let newtext = embedExists.description.replace("$text", obj.$text.value).replace("$userID", obj.$userID.value).replace("$serverID", obj.$serverID.value).replace("$user", obj.$user.value).replace("$channel", obj.$channel.value).replace("$nl", obj.$nl.value)
 
-            if(embedExists.permission === "staff") {
-                if(!message.member.permissions.has("MANAGE_MESSAGES")) return;
-            }
 
-            if(embedExists.permission === "admin") {
-                if(!message.member.permissions.has("ADMINISTRATOR")) return;
-            }
-
-            if(embedExists.guild === message.guild.id) {
-                let embed = new MessageEmbed()
-                .setDescription(`${embedExists.description}`)
+        if (embedExists.guild === message.guild.id) {
+            let embed = new MessageEmbed()
+                .setDescription(`${newtext}`)
                 .setColor("RANDOM")
 
-                if(embedExists.title) {
-                    embed.setTitle(`${embedExists.title}`)
-                }
-                
-                if(embedExists.deltrig === true) {
-                    message.delete();
-                    message.channel.send({ embeds: [embed] });
-                    return;
-                } else {
-                    message.reply({ embeds: [embed] });
-                }
-    
+            if (embedExists.title) {
+                embed.setTitle(`${embedExists.title}`)
+            }
+
+            if (embedExists.deltrig === true) {
+                message.delete();
+                message.channel.send({ embeds: [embed] });
+                return;
+            } else {
+                message.reply({ embeds: [embed] });
             }
 
         }
+
+    }
 
 }
