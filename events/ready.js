3a9afdeed08fd3b0c184844b8043f63e9c33bf16
Dez P <63563412+scalgoon@users.noreply.github.com>
2022-03-17 10:44:12 -0500

    Added Mongo connection

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,18 @@
-module.exports = (client) => {
-    client.log('Bot', 'Goot is online');
+const { table } = require('table');
+const { promisify } = require("util");
+const readdir = promisify(require('fs').readdir);
+const mongoose = require('mongoose');
+
+require('colors');
+require('dotenv').config();
+
+module.exports = async (client) => {
+    
+    let on = "Online"
+
+    const evtFiles = await readdir('./events')
+    
+    console.log(`${table([[`${client.user.username} is ready!`,`Status: ${on.green} | Servers: ${client.guilds.cache.size.toString().yellow} | Events: ${evtFiles.length.toString().yellow} | Commands: ${client.commands.size.toString().yellow}`]])}`);
 
     client.user.setPresence({
         status: 'idle',
@@ -7,6 +20,14 @@ module.exports = (client) => {
             name: `myself be made 👀`,
             type: "WATCHING",
         }],
-}, 120000);
+    });
+    
+    //In your .env file you will replace "DATABASE_URL" with "database" and there place your mongo connection string.
+
+    await mongoose.connect(process.env.database || '', {
+        keepAlive: true
+    });
+
+    client.log("Mongo", "Database connected successfully");
 
 }
