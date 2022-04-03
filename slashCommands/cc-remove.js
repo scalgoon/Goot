75cee43749e32db99cf2924543be66088f7896a4
Dez P <63563412+scalgoon@users.noreply.github.com>
2022-04-03 08:00:57 -0500

    Better command

--- slashCommands/cc-remove.js
+++ slashCommands/cc-remove.js
@@ -5,105 +5,58 @@ const { MessageEmbed, Permissions } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('cc-remove')
-        .setDescription('Creates a custom command')
-        .addSubcommand(subcommand => subcommand
-            .setName('text')
-            .setDescription('Remove a text command')
-            .addStringOption(option => option.setName('name').setDescription('Enter the name of command').setRequired(true)))
-        .addSubcommand(subcommand => subcommand
-            .setName('embed')
-            .setDescription('Remove a embed command')
-            .addStringOption(option => option.setName('name').setDescription('Enter the name of command').setRequired(true))),
+        .setDescription('Removes a custom command')
+        .addStringOption(option => option.setName('name').setDescription('Enter the name of command').setRequired(true)),
     async execute(client, interaction, prisma) {
 
         let noperm = new MessageEmbed()
-      .setDescription(`<:cross:782029257739599873> You do not have permission to use this command!`)
-      .setColor("RED")
+            .setDescription(`<:cross:782029257739599873> You do not have permission to use this command!`)
+            .setColor("RED")
 
-      if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
-        return interaction.reply({ embeds: [noperm], ephemeral: true })
-      }
-
-        if (interaction.options.getSubcommand() === 'text') {
-            const name = interaction.options.getString('name')
-
-            let triggertouse = `!${name}`
-
-            const exists = await prisma.cCommands.findUnique({
-                where: {
-                    placeholder: `${name + interaction.guild.id}`,
-                },
-                select: {
-                    guild: true
-                }
-            })
-
-            if (exists) {
-
-                if (exists.guild === interaction.guild.id) {
-                    await prisma.cCommands.delete({
-                        where: {
-                            placeholder: `${name + interaction.guild.id}`,
-                        }
-                    })
-
-                    let embed = new MessageEmbed()
-                        .setTitle("Command Removed")
-                        .setDescription(`<:check:782029189963710464> Successfully deleted command with trigger **${triggertouse}**`)
-                        .setColor("GREEN")
-
-                    interaction.reply({ embeds: [embed] })
-                }
-            } else {
-                let embed = new MessageEmbed()
-                    .setTitle("Command Error")
-                    .setDescription(`Couldn't find that command in the database!`)
-                    .setColor("RED")
-
-                interaction.reply({ embeds: [embed], ephemeral: true})
-            }
-
+        if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
+            return interaction.reply({ embeds: [noperm] });
         }
 
-        if (interaction.options.getSubcommand() === 'embed') {
-            const name = interaction.options.getString('name')
+        const name = interaction.options.getString('name')
 
-            let triggertouse = `!${name}`
+        let triggertouse = `!${name}`
 
-            const exists = await prisma.cCEmbeds.findUnique({
-                where: {
-                    placeholder: `${name + interaction.guild.id}`,
-                },
-                select: {
-                    guild: true
-                }
-            })
-
-            if (exists) {
-
-                if (exists.guild === interaction.guild.id) {
-                    await prisma.cCEmbeds.delete({
-                        where: {
-                            placeholder: `${name + interaction.guild.id}`,
-                        }
-                    })
-
-
-                    let embed = new MessageEmbed()
-                        .setTitle("Command Removed")
-                        .setDescription(`<:check:782029189963710464> Successfully deleted command with trigger **${triggertouse}**`)
-                        .setColor("GREEN")
-
-                    interaction.reply({ embeds: [embed] })
-                }
-            } else {
-                let embed = new MessageEmbed()
-                    .setTitle("Command Error")
-                    .setDescription(`Couldn't find that command in the database!`)
-                    .setColor("RED")
-    
-                interaction.reply({ embeds: [embed],ephemeral: true })
+        const exists = await prisma.commands.findUnique({
+            where: {
+                placeholder: `!${name + interaction.guild.id}`,
+            },
+            select: {
+                guild: true
             }
-        } 
+        })
+
+        if (exists) {
+
+            if (exists.guild === interaction.guild.id) {
+                await prisma.commands.delete({
+                    where: {
+                        placeholder: `!${name + interaction.guild.id}`,
+                    }
+                })
+
+                let embed = new MessageEmbed()
+                    .setTitle("Command Removed")
+                    .setDescription(`<:check:782029189963710464> Successfully deleted command with trigger **${triggertouse}**`)
+                    .setColor("GREEN")
+
+                try {
+                    await interaction.reply({ embeds: [embed] })
+                } catch (e) {
+                    return;
+                }
+            }
+        } else {
+            let embed = new MessageEmbed()
+                .setTitle("Command Error")
+                .setDescription(`Couldn't find that command in the database!`)
+                .setColor("RED")
+
+            interaction.reply({ embeds: [embed], ephemeral: true })
+        }
     },
 };
