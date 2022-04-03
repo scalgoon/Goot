65c1cc9a7ffda8031570a3593f646f6e1aa34398
Dez P <63563412+scalgoon@users.noreply.github.com>
2022-04-03 08:01:51 -0500

    Better command

--- slashCommands/help.js
+++ slashCommands/help.js
@@ -7,112 +7,80 @@ module.exports = {
         .setDescription('Get a list of the bot\'s commands')
         .addStringOption(option => option.setName("command").setDescription("Specify a command to get info for")),
     async execute(client, interaction, prisma) {
-        const textExists = await prisma.cCommands.findMany({
-            where: {
-                guild: interaction.guild.id
-            },
-            select: {
-                id: true,
-                placeholder: true,
-                permission: true,
-                text: true
-            }
-        })
 
-        const embedExists = await prisma.cCEmbeds.findMany({
+
+        const textExists = await prisma.commands.findMany({
             where: {
                 guild: interaction.guild.id
             },
             select: {
-                id: true,
                 placeholder: true,
                 permission: true,
-                description: true
+                text: true,
+                description: true,
+                title: true,
+                trigger: true,
             }
         })
 
         const cmd = interaction.options.getString('command')
 
-        if(cmd) {
+        if (cmd) {
 
-            const textcmd = await prisma.cCommands.findUnique({
+            const thecmd = await prisma.commands.findUnique({
                 where: {
-                    placeholder: `${cmd + interaction.guild.id}`
+                    placeholder: `!${cmd + interaction.guild.id}`
                 },
                 select: {
                     text: true,
                     guild: true,
                     deltrig: true,
                     permission: true,
-                }
-            })
-
-            const newembed = await prisma.cCEmbeds.findUnique({
-                where: {
-                    placeholder: `${cmd + interaction.guild.id}`
-                },
-                select: {
-                    description: true,
-                    guild: true,
                     title: true,
-                    deltrig: true,
-                    permission: true,
+                    description: true,
                 }
             })
 
 
-            if(newembed) {
+            if (thecmd) {
                 let helpbed = new MessageEmbed()
-                .setTitle(`Help for ${cmd}`)
-                .addField("Permission", `${newembed.permission}`)
-                .addField("Sends", `${newembed.description}`)
-                .setColor("RANDOM")
+                    .setAuthor(`Information about ${cmd}`, client.user.displayAvatarURL())
+                    .addField("Permission", `${thecmd.permission}`, true)
+                    .addField("Delete trigger", `${thecmd.deltrig}`, true)
+                    .addField("Sends", `${thecmd.description || thecmd.text}`, true)
+                    .setColor("RANDOM")
+
+                if (thecmd.title) {
+                    helpbed.addField("Title", `${thecmd.title}`)
+                }
 
                 interaction.reply({ embeds: [helpbed] });
-            } else if(textcmd) {
-                let shelpbed = new MessageEmbed()
-                .setTitle(`Help for ${cmd}`)
-                .addField("Permission", `${textcmd.permission}`)
-                .addField("Sends", `${textcmd.text}`)
-                .setColor("RANDOM")
-
-                interaction.reply({ embeds: [shelpbed] });
             } else {
                 let nobed = new MessageEmbed()
-                .setDescription(`<:cross:782029257739599873> That command doesn't exist in this guild!`)
-                .setColor("RED")
+                    .setDescription(`<:cross:782029257739599873> That command doesn't exist in this guild!`)
+                    .setColor("RED")
 
                 interaction.reply({ embeds: [nobed], ephemeral: true })
             }
         }
 
-        let cmds = embedExists.map((x) => `\`${x.id}\``).join(", ")
-        let cmd2 = textExists.map((v) => `\`${v.id}\``).join(", ")
-
-        let ar;
-
-        if (cmds && cmd2) {
-            ar = `${cmds}, ${cmd2}`
-        } else if (cmds && !cmd2) {
-            ar = `${cmds}`
-        } else {
-            ar = `${cmd2}`
-        }
+        let cmds = textExists.map((x) => `\`${x.trigger}\``).join(", ")
 
         let embed = new MessageEmbed()
             .setTitle("Help Embed")
+            .setDescription("Here is a list of commands you can use in your server. Do **/help <command>** to learn more about a specific command.")
             .addField("<:slash:908546180265422909> Slash Commands", client.commands.map(c => '`' + c.data.name + '`').join(', '))
             .setColor("RANDOM")
 
-        if (ar) {
-            embed.addField("<:legacy:908546232127979570> Custom Commands", `${ar}`)
+        if (cmds) {
+            embed.addField("<:legacy:908546232127979570> Custom Commands", `${cmds}`)
         }
 
-         try{
-           await interaction.reply({ embeds: [embed] })
-              }catch(e){
-                return;
-              }
+        try {
+            await interaction.reply({ embeds: [embed] })
+        } catch (e) {
+            return;
+        }
 
     },
 };
