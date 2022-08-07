a0c57877e9175144d94efc857816744e7ba01da6
Dez P <63563412+scalgoon@users.noreply.github.com>
2022-08-07 18:27:50 -0500

    v14

--- slashCommands/invite.js
+++ slashCommands/invite.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,10 +7,10 @@ module.exports = {
 		.setDescription('Used to invite the bot to your server'),
 	async execute(client, interaction, prisma) {
 		
-        let invbed = new MessageEmbed()
+        let invbed = new EmbedBuilder()
         .setTitle("Thanks for choosing me!")
         .setDescription("You can invite me [here](https://discord.com/api/oauth2/authorize?client_id=784903173767823370&permissions=8&scope=applications.commands%20bot)")
-        .setColor("GREEN")
+        .setColor("Green")
 
         interaction.reply({ embeds: [invbed] })
 
