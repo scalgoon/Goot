60ffed7954717e104dad1e5abd4908a8b81b28db
Dez P <63563412+scalgoon@users.noreply.github.com>
2022-08-07 18:27:23 -0500

    v14

--- slashCommands/help.js
+++ slashCommands/help.js
@@ -1,79 +1,55 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+const db = require('quick.db');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('Get a list of the bot\'s commands')
         .addStringOption(option => option.setName("command").setDescription("Specify a command to get info for")),
-    async execute(client, interaction, prisma) {
-
-
-        const textExists = await prisma.commands.findMany({
-            where: {
-                guild: interaction.guild.id
-            },
-            select: {
-                placeholder: true,
-                permission: true,
-                text: true,
-                description: true,
-                title: true,
-                trigger: true,
-            }
-        })
+    async execute(client, interaction) {
 
         const cmd = interaction.options.getString('command')
 
         if (cmd) {
 
-            const thecmd = await prisma.commands.findUnique({
-                where: {
-                    placeholder: `!${cmd + interaction.guild.id}`
-                },
-                select: {
-                    text: true,
-                    guild: true,
-                    deltrig: true,
-                    permission: true,
-                    title: true,
-                    description: true,
-                }
-            })
+            const thecmd = db.fetch(`!${cmd}-${interaction.guild.id}`);
 
 
             if (thecmd) {
-                let helpbed = new MessageEmbed()
-                    .setAuthor(`Information about ${cmd}`, client.user.displayAvatarURL())
-                    .addField("Permission", `${thecmd.permission}`, true)
-                    .addField("Delete trigger", `${thecmd.deltrig}`, true)
-                    .addField("Sends", `${thecmd.description || thecmd.text}`, true)
-                    .setColor("RANDOM")
+                let helpbed = new EmbedBuilder()
+                    .setAuthor({ name: `Information about ${cmd}`, iconURL: client.user.displayAvatarURL() })
+                    .addFields(
+                        { name: "Permission", value: thecmd.perms, inline: true },
+                        { name: "Delete trigger", value: `${thecmd.deltrig}`, inline: true },
+                        { name: "Sends", value: thecmd.description || thecmd.text, inline: true }
+                    )
+                    .setColor("Random")
 
                 if (thecmd.title) {
-                    helpbed.addField("Title", `${thecmd.title}`)
+                    helpbed.addFields({ name: "Title", value: `${thecmd.title}` })
                 }
 
                 interaction.reply({ embeds: [helpbed] });
             } else {
-                let nobed = new MessageEmbed()
+                let nobed = new EmbedBuilder()
                     .setDescription(`<:cross:782029257739599873> That command doesn't exist in this guild!`)
-                    .setColor("RED")
+                    .setColor("Red")
 
                 interaction.reply({ embeds: [nobed], ephemeral: true })
             }
         }
 
-        let cmds = textExists.map((x) => `\`${x.trigger}\``).join(", ")
+        let cmds = await db.get(`helpcmd-${interaction.guild.id}`);
 
-        let embed = new MessageEmbed()
+        let embed = new EmbedBuilder()
             .setTitle("Help Embed")
             .setDescription("Here is a list of commands you can use in your server. Do **/help <command>** to learn more about a specific command.")
-            .addField("<:slash:908546180265422909> Slash Commands", client.commands.map(c => '`' + c.data.name + '`').join(', '))
-            .setColor("RANDOM")
+            .addFields({ name: "<:slash:908546180265422909> Slash Commands", value: client.commands.map(c => '`' + c.data.name + '`').join(', ') })
+            .setColor("Random")
 
         if (cmds) {
-            embed.addField("<:legacy:908546232127979570> Custom Commands", `${cmds}`)
+            embed.addFields({ name: "<:legacy:908546232127979570> Custom Commands", value: `${cmds}` })
         }
 
         try {
