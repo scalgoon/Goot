5e34bb6ade65605c3c7a7c709fefdabff48b7b32
Dez P <63563412+scalgoon@users.noreply.github.com>
2022-08-07 18:31:42 -0500

    v14

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,13 +1,11 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = async (client, guild) => {
 
   let embed = new MessageEmbed()
 		.setTitle("Hi i'm Goot!")
 		.setDescription("I'm a bot centered around custom fearures!\nTo get started do <:slash:908546180265422909> to see all my commands.\n\nCustom commands work with the prefix <:legacy:908546232127979570>")
-		.setColor("RANDOM")
+		.setColor("Random")
 
 
     if (guild.systemChannel) {
@@ -15,13 +13,4 @@ module.exports = async (client, guild) => {
     }
 
     console.log(`Server joined: ${guild.name}`);
-
-        await prisma.guild.create({   
-          data: {
-            name: guild.name,
-            id: guild.id
-          },      
-        })
-
-        client.log("Prisma", `Guild created with id: ${guild.id}`)
 };
