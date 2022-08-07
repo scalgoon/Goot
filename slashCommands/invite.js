a1b907bab7dc2612d0986f3d2d52d8292e2b33b5
Dez P <63563412+scalgoon@users.noreply.github.com>
2022-08-07 18:28:18 -0500

    v14

--- slashCommands/invite.js
+++ slashCommands/invite.js
@@ -1,18 +1,44 @@
 const { SlashCommandBuilder } = require('discord.js');
+
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('invite')
-		.setDescription('Used to invite the bot to your server'),
-	async execute(client, interaction, prisma) {
-		
-        let invbed = new EmbedBuilder()
-        .setTitle("Thanks for choosing me!")
-        .setDescription("You can invite me [here](https://discord.com/api/oauth2/authorize?client_id=784903173767823370&permissions=8&scope=applications.commands%20bot)")
-        .setColor("Green")
+		.setName('ping')
+		.setDescription('Sends the bot\'s ping and latency'),
+	async execute(client, interaction) {
+		let ping = new EmbedBuilder()
+        .setTitle(`Calculating ping...`)
+    
+        const startTime = Date.now();
+        interaction.reply({ embeds: [ping] })
+        .then(msg => {
+            const endTime = Date.now();
+            var pong = endTime-startTime;
 
-        interaction.reply({ embeds: [invbed] })
+            let apipong = Math.round(client.ws.ping);
 
-    }
+            let emoj;
+
+            let colo;
+
+            if (apipong < 60) {
+              emoj = "<:3bar:908545977177214977>"
+              colo = "Green"
+            } else if (apipong > 60 && apipong < 100) {
+              emoj = "<:2bar:908546068038422628>"
+              colo = "Orange"
+            } else {
+              emoj = "<:1bar:908546024115695656>"
+              colo = "Red"
+            }
+    
+                let ping2 = new EmbedBuilder()
+                .setTitle(`${emoj} Pong!`)
+                .setColor(colo)
+                .setDescription(`Latency: ${pong} ms \nApi Latency: ${apipong} ms` )
+    
+                interaction.editReply({ embeds: [ping2] });
+		});
+	},
 };
